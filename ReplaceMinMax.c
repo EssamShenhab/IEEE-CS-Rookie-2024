d7ec48bd807c161dbfee83f830080554c205a3c2
EssamShenhab <152318011+EssamShenhab@users.noreply.github.com>
2023-11-30 23:54:27 +0200

    Create ReplaceMinMax.c

---
+++ ReplaceMinMax.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int findMinIndex(int arr[], int n) {
+    int minIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+int findMaxIndex(int arr[], int n) {
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int minIndex = findMinIndex(arr, n);
+    int maxIndex = findMaxIndex(arr, n);
+
+    swap(arr, minIndex, maxIndex);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
