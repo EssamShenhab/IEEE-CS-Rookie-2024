82f85539d2b5ff93dc9ab31ad00764b8d2442b5b
EssamShenhab <152318011+EssamShenhab@users.noreply.github.com>
2023-12-12 23:18:59 +0200

    Delete B_print.py

--- B_print.py
+++
@@ -1,9 +0,0 @@
-def print_numbers(N):
-    result = ""
-    for i in range(1, N + 1):
-        result += str(i) + " "
-    return result.strip()
-
-N = int(input().strip())
-
-print(print_numbers(N))
