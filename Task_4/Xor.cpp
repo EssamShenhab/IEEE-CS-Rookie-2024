05d76a1c4a38ef26bf34a049df844172c819ab78
EssamShenhab <152318011+EssamShenhab@users.noreply.github.com>
2023-12-13 18:24:00 +0200

    Xor.cpp

---
+++ Task_4/Xor.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long a,b,q;
+    cin>>a>>b>>q;
+    long long result = q % 3;
+    if(result == 1){
+        cout<<a;
+    } else if (result == 2){
+        cout<<b;
+    } else {
+        cout<<(a ^ b)<<endl;
+    }
+}
